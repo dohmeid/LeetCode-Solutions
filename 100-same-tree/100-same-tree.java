a69238bebe61661f60691b18c56464e88c80bc60
Doha Hmeid <doha.hmeid@gmail.com>
2022-09-06 23:45:20 +0300

    Time: 0 ms (100.00%), Space: 42.1 MB (11.90%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -19,45 +19,14 @@ class Solution {
     
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p==null && q==null) return true;
-        if(p!=null && q==null) return false;
-        if(p==null && q!=null) return false;
+        if(p==null || q==null) return false;
         if(p.val != q.val) return false;
             
-        traverse(p,plist);
-        traverse(q,qlist);
-         if(plist.size() != qlist.size())return false;
-        for(int i=0;i<plist.size();i++){
-            TreeNode l1 = plist.get(i);
-            TreeNode l2 = qlist.get(i); 
-           
-            if(l1 != null && l2==null)
-                return false;
-            else if(l1 == null && l2!=null)
-                return false;
-            else if(l1 == null && l2==null)
-                continue;
-            else if( l1.val != l2.val )
-                return false;
-        }
+        boolean lres = isSameTree(p.left,q.left);
+        boolean rres = isSameTree(p.right,q.right);
         
-      //  if(plist.equals(qlist) == true)
-            return true;
-       
-    }
-    
-    
-    
-     public void traverse(TreeNode t,ArrayList<TreeNode> l){ //in-order traversal : left then root then right
-         
-         if(t.left != null)
-           traverse(t.left,l);
-         l.add(t.left);
-         
-         if(t.right != null)
-            traverse(t.right,l);
-         
-         l.add(t.right);
         
-     } 
+        
+       return lres && rres; 
     
-}
+}}
