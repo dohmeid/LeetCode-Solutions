e8a648b95b668470dd80f648abe3f8f54c18f2c9
Doha Hmeid <doha.hmeid@gmail.com>
2022-09-09 23:29:24 +0300

    Time: 120 ms (83.76%), Space: 89 MB (93.29%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -4,9 +4,11 @@ class Solution {
         int rows = properties.length;
         int w=0;
         
-         //Arrays.sort(properties, 0, );
-         Arrays.sort(properties, (a,b) -> (a[0]!=b[0]) ? b[0]-a[0] : a[1]-b[1]);
+        //sort the array in a decreasing order based on the first column (attack) ->  b[0]-a[0]
+        //if attacks are equal, sort them based on the defense -> a[1]-b[1]
+        Arrays.sort(properties, (a,b) -> (a[0]!=b[0]) ? b[0]-a[0] : a[1]-b[1]);
          
+         // compare on the second column (defense)
          int max=0;
          for(int i=0;i<rows;i++){
              if(properties[i][1] < max)
