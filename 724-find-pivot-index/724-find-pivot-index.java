c7c8a67cfb5408915ab4555a8a4ac9e324f99bee
Doha Hmeid <doha.hmeid@gmail.com>
2022-09-10 23:18:08 +0300

    Time: 2 ms (78.77%), Space: 53 MB (30.26%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -1,25 +1,37 @@
 class Solution {
     public int pivotIndex(int[] nums) {
+        int in= -1;  
         
-        int in= -1;
-        
+       /*  solution 1
         for(int i=0;i<nums.length;i++){
-             int sleft=0;
-             int sright=0;
-            for(int j=0; j<i ; j++){
+            int sleft=0, sright=0;
+            for(int j=0; j<i ; j++)
                 sleft += nums[j];
-            }
-            
-             for(int j=i+1; j<nums.length ; j++){
+            for(int j=i+1; j<nums.length ; j++)
                 sright += nums[j];
-            }
             
             if(sleft == sright){
                in = i;
                 break;
             }
-            
         }
+        */
+        
+        //solution 2 - better than 1
+        int sum=0;
+        for(int i=0;i<nums.length;i++)
+            sum += nums[i];
+        
+        int sleft=0;
+        for(int i=0;i<nums.length;i++){
+            if(sleft == sum - nums[i] - sleft){
+               in = i;
+                break;
+            }
+            else
+                sleft += nums[i];
+        }
+        
         
         return in;
     }
