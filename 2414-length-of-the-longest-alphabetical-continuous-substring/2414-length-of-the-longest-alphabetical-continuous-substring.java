007a5dd5ff70f00a3c406723d2fb43d71ebcd95c
Doha Hmeid <doha.hmeid@gmail.com>
2022-09-19 20:04:37 +0300

    Time: 25 ms (11.11%), Space: 53.5 MB (66.67%) - LeetHub

--- 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.java
+++ 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.java
@@ -4,13 +4,9 @@ class Solution {
         int len=1,max=0;
         
         for(int i=0; i<s.length()-1;i++){
-            char c1 =  (s.charAt(i));
-            char c2 = (s.charAt(i+1));
-            int n1 = c1;
-            int n2 = c2;
-            if(n1+1 == n2){
+           
+            if(s.charAt(i)+1 == s.charAt(i+1))
                 len++;
-            }
             else{
                 max  = Math.max(len,max);
                 len=1;
