29776f98a0b3387087ea0ed95036e54ff8a9897d
Doha Hmeid <doha.hmeid@gmail.com>
2022-09-21 00:44:48 +0300

    Time: 56 ms (19.95%), Space: 86.1 MB (51.58%) - LeetHub

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.java
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.java
@@ -4,11 +4,26 @@ class Solution {
         int res=0,flag=0;
         Arrays.sort(players);  
         Arrays.sort(trainers);   //sort arrays  in ascending Order
-       
+     
+        
+        //solution1
+        int index1=0 , index2=0;
+         while( index1<players.length && index2<trainers.length ){
+             if(players[index1] <= trainers[index2]){
+                 index1++;
+                 index2++;
+                 res++;
+             }
+             else{
+                 index2++;
+             }
+             
+         }
+        
+        /* solution2
         for(int i=0; i<players.length ;i++){
             int index1=i;
             int index2=flag;
-            
             while(index2<trainers.length ){
                 if(players[index1] <= trainers[index2]){
                     trainers[index2] = 0;
@@ -20,6 +35,7 @@ class Solution {
             }
             
         }
+        */ 
         
         return res;
     }
