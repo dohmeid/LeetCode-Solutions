aa5dff34608b0fb479b05e4a78e8998da95fe78e
Doha Hmeid <doha.hmeid@gmail.com>
2022-09-21 20:40:53 +0300

    Time: 11 ms (19.61%), Space: 69.1 MB (67.85%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -9,7 +9,6 @@ class Solution {
         }
         
         for(int i=0; i<q.length ; i++){
-            
             int val = q[i][0];
             int index = q[i][1];
             int old = nums[index];
@@ -17,30 +16,11 @@ class Solution {
             
             if(old%2==0)
                even -= old; 
-            if(nums[index]%2==0){
+            if(nums[index]%2==0)
                 even += nums[index]; 
-            }
-          
             
             sum[i] = even;
-            
         }
-        
-        
         return sum;
     }
 }
-
-
-/*
-     j
-     0 1
- i   ---
- 0   1 0
- 1  -3 1
- 2  -4 0
- 3   2 3
-
-nums[0] += q[][]
-
-*/
