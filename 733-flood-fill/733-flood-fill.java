abf7b1659ebd38f86581fe07d21c545ca56532bb
Doha Hmeid <doha.hmeid@gmail.com>
2022-09-25 01:23:46 +0300

    Time: 1 ms (91.13%), Space: 48.1 MB (48.75%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,26 +1,25 @@
 class Solution {
-    int [][] visited = new int[50][50];  
+    
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         
-        if( visited[sr][sc]!=1){
-        
         int old_color = image[sr][sc];
-        image[sr][sc] = color;
-        visited[sr][sc]=1; //to avoid infinit loops -_-
+        if(old_color == color)
+            return image;
        
-        
-        if(sc+1<image[0].length && image[sr][sc+1] == old_color)
-            image = floodFill(image,sr,sc+1,color);
-        if(sc-1>=0 && image[sr][sc-1] == old_color)
-            image = floodFill(image,sr,sc-1,color);
-        
-        if(sr+1<image.length  && image[sr+1][sc] == old_color)
-            image = floodFill(image,sr+1,sc,color);
-        if( sr-1>=0 && image[sr-1][sc] == old_color)
-            image = floodFill(image,sr-1,sc,color);
-        
-        }
+        dfs(image, sr, sc, old_color, color);
         
         return image;
     }
+    
+    private void dfs(int[][] image, int sr, int sc, int old_color, int new_color){
+        if(sc<image[0].length && sc>=0 && sr<image.length && sr>=0 && image[sr][sc]==old_color ) {
+            image[sr][sc] = new_color;
+            dfs(image,sr, sc+1, old_color, new_color);
+            dfs(image,sr, sc-1, old_color, new_color);
+            dfs(image,sr+1, sc, old_color, new_color);
+            dfs(image,sr-1, sc, old_color, new_color);
+        }
+    }
+    
+    
 }
