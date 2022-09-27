d0be8067fe71876870e786db961b55c03709f1a7
Doha Hmeid <doha.hmeid@gmail.com>
2022-09-28 00:55:57 +0300

    Time: 10 ms (8.90%), Space: 42.9 MB (6.03%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String defangIPaddr(String a) {
+        String res="";
+        for(int i=0 ; i<a.length();i++){
+            if(a.charAt(i)!='.')
+                res += a.charAt(i);
+            else
+                res += "[.]";
+        }
+    
+        return res;
+    }
+}
