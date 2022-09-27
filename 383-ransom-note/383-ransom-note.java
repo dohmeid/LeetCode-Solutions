3aa32b255ef7ce9406745c9db6929c36e4a31b57
Doha Hmeid <doha.hmeid@gmail.com>
2022-09-27 18:26:58 +0300

    Time: 34 ms (21.83%), Space: 51.9 MB (26.68%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean canConstruct(String rn, String m) {
         
-        Map<Character, Integer> m1 = new HashMap<>();
+       // Map<Character, Integer> m1 = new HashMap<>();
         Map<Character, Integer> m2 = new HashMap<>();
         
-        for(int i=0; i<rn.length();i++){
+      /*  for(int i=0; i<rn.length();i++){
             char c = rn.charAt(i);
              if (!m1.containsKey(c))
                  m1.putIfAbsent(c, 1); //key = char , value=frequancy of the char
@@ -13,7 +13,7 @@ class Solution {
                 m1.put(c, ++v);
             }   
             
-        }
+        }*/
         
         for(int i=0; i<m.length();i++){
             char c = m.charAt(i);
@@ -26,13 +26,26 @@ class Solution {
             
         }
         
-        for (Map.Entry<Character, Integer> entry : m1.entrySet()) {
+        for(int i=0; i<rn.length();i++){
+            char c = rn.charAt(i);
+            if (!m2.containsKey(c))
+                return false;
+            
+            int v = m2.get(c);
+            if(v==0)
+                return false;
+            m2.put(c, --v);
+                
+                
+        
+        }
+    /*    for (Map.Entry<Character, Integer> entry : m1.entrySet()) {
             char c = entry.getKey();
             int val = entry.getValue();
             if(!m2.containsKey(c) || m2.get(c)<val)
                 return false;
             
-        }
+        }*/
         
         
         return true;
