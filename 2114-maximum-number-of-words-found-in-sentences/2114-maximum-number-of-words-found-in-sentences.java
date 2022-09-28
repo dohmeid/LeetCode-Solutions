1159010f5a589e6551383c5ddd77d2f23bcdf1ee
Doha Hmeid <doha.hmeid@gmail.com>
2022-09-28 22:15:18 +0300

    Time: 3 ms (96.60%), Space: 42 MB (90.02%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -2,19 +2,10 @@ class Solution {
     public int mostWordsFound(String[] s) {
         
         int max=0;
-        
         for(int i=0; i<s.length ; i++){
-            
-            String sen = s[i];
-            String[] words = sen.split("\\s+"); //This solution uses the regular expression "\\s+" to split the string on whitespace.
+            String[] words =  s[i].split(" "); 
             max = Math.max(max, words.length);
-            
-            
-        }
-        
-        
-        
-        
+        }        
         return max;
     }
 }
