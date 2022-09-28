4a10728152293bb89815af4cd86f1c9071364eaa
Doha Hmeid <doha.hmeid@gmail.com>
2022-09-28 16:20:37 +0300

    Time: 10 ms (63.38%), Space: 54 MB (66.65%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -2,18 +2,11 @@ class Solution {
     public int minPartitions(String n) {
         
         int max = 0;
-        String s = "";
         for(int i=0 ; i<n.length();i++){
-            
             char c = n.charAt(i);
-            s += c;
-            int num = Integer.parseInt(s);
+            int num = c - '0'; // Converting character to its integer value
             max = Math.max(max,num);
-            s="";
         }
-        
-        
-        
         return max;
     }
     
