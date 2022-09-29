90809d881fa77b59d48f08a1ab88c49adc248076
Doha Hmeid <doha.hmeid@gmail.com>
2022-09-29 18:12:34 +0300

    Time: 1 ms (90.42%), Space: 41.6 MB (47.06%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -2,12 +2,31 @@ class Solution {
     public int numIdenticalPairs(int[] nums) {
         
         int res=0;
+       
+        /* sol1
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[j]==nums[i] && i<j)
                     res++;
             }
         }
+        */
+        
+        //sol2
+        HashMap<Integer, Integer> h = new HashMap<>(); //key is the number , value is the frequancy
+         for(int n : nums){
+            if(!h.containsKey(n)) {
+                h.put(n, 0);
+            }
+             else{
+                 int count = h.get(n);
+                 h.put(n, ++count);
+                 res += count;
+             }
+
+             
+             
+         }
         
         return res;
     }
