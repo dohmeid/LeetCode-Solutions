791270b37f61e4fa8de462070c39829ff0773d5b
Doha Hmeid <doha.hmeid@gmail.com>
2022-09-30 18:31:13 +0300

    Time: 14 ms (69.43%), Space: 44.5 MB (62.58%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<10 && x>=0)
+            return true;
+        if(x%10==0 || x<0 )
+            return false;
+        
+      int res = 0;
+       while(x>res){
+        res = res*10 + x%10;
+        x = x/10;
+       }
+    return (x==res || x==res/10);
+    }
+}
