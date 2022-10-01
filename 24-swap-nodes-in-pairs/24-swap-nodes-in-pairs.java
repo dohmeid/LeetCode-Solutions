4091f967b551d858e4f16e12c38fb977cef9a652
Doha Hmeid <doha.hmeid@gmail.com>
2022-10-02 00:14:11 +0300

    Time: 0 ms (100.00%), Space: 39.6 MB (98.47%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
@@ -14,10 +14,13 @@ class Solution {
         if(head==null || head.next==null)
             return head;
 
-        int v1 = head.val;
-        head.val = head.next.val;
-        head.next.val = v1;
-        swapPairs( head.next.next);
+        ListNode n1 = head;
+        ListNode n2 = head.next;
+        ListNode n3 = head.next.next;
+        head = n2;
+        head.next = n1;
+        
+        head.next.next = swapPairs(n3);
         
         return head;
     }
