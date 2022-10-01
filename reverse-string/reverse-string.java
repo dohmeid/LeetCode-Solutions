93e0b47389cd019bffe5cc2f1cc934c811579318
Doha Hmeid <doha.hmeid@gmail.com>
2022-10-01 23:00:16 +0300

    Time: 3 ms, Memory: 55.5 MB - LeetHub

---
+++ reverse-string/reverse-string.java
@@ -0,0 +1,41 @@
+class Solution {
+    public void reverseString(char[] s) {
+    
+        /* sol1 : using stack
+        
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<s.length;i++){
+            st.push(s[i]);
+        }
+        
+        for(int i=0;i<s.length;i++){
+            s[i] = st.pop();
+        }
+       */
+        
+        
+       /* sol2: using 2 pointers - less time
+        for(int i=0;i<s.length/2;i++){
+            char c = s[s.length-i-1];
+            s[s.length-i-1] = s[i];
+            s[i] = c; 
+        }
+        */
+        
+        
+        //sol3 : using recursion
+            rec(s,0);
+        
+      
+    }
+    
+     public void rec(char[] s,int i) {
+         if (s == null || i >= s.length/2) 
+            return;
+         
+         char temp = s[i];
+         s[i] = s[s.length-i-1];
+         s[s.length-i-1] = temp;
+          rec(s,i+1);
+     }   
+}
