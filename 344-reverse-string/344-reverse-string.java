1cebcde06edca4ff34c4d2524857124d97e9e7d5
Doha Hmeid <doha.hmeid@gmail.com>
2022-10-01 22:20:03 +0300

    Time: 1 ms (99.91%), Space: 48.8 MB (98.44%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -1,6 +1,7 @@
 class Solution {
     public void reverseString(char[] s) {
-        
+    
+        /* sol1 : using stack
         Stack<Character> st = new Stack<>();
         for(int i=0;i<s.length;i++){
             st.push(s[i]);
@@ -9,7 +10,18 @@ class Solution {
         for(int i=0;i<s.length;i++){
             s[i] = st.pop();
         }
-       
+       */
+        
+       //sol2:
+        for(int i=0;i<s.length/2;i++){
+            char c = s[s.length-i-1];
+            
+            s[s.length-i-1] = s[i];
+            s[i] = c;
+            
+            
+        }
+        
       
     }
 }
