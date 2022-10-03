a56431e784163a79b687b2101586b5ada0737dd1
Doha Hmeid <doha.hmeid@gmail.com>
2022-10-03 14:23:03 +0300

    Time: 4 ms (11.15%), Space: 41.5 MB (68.40%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        
+        ArrayList<List<Integer>> l = new ArrayList<List<Integer>>();
+        int r=0 , c=0 ;
+        int n = rowIndex;
+        while(rowIndex>=0){
+            
+            List<Integer> l2 = new ArrayList<Integer>();
+            for(int i=0; i<c+1; i++){
+                if(i==0 || i==r)
+                    l2.add(1);
+                else{
+                    List<Integer> l3 = l.get(r-1);
+                    int sum = l3.get(i) + l3.get(i-1);
+                    l2.add(sum);
+                }
+            }
+            
+            l.add(l2);
+            c++;
+            r++;
+            rowIndex--;
+        }
+     
+        return l.get(n);
+    }
+    
+}
