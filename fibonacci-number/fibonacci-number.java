53a7bafe34785b725c993f3d84f2f5d345d71c71
Doha Hmeid <doha.hmeid@gmail.com>
2022-10-04 23:33:44 +0300

    Time: 1 ms, Memory: 40.1 MB - LeetHub

---
+++ fibonacci-number/fibonacci-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    //solution is using recursion with memoization 
+    HashMap<Integer, Integer> cache = new HashMap<Integer, Integer>();
+    public int fib(int n) {
+        
+        if(cache.containsKey(n))
+            return cache.get(n);
+        
+        int result=0;
+        if(n<2)
+            result = n;
+        else
+            result = fib(n-1)+fib(n-2);
+        cache.put(n,result);
+        
+        return result;
+    }
+}
