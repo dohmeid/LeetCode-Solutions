6975df0e30112d06ff40356cc1188a0b7a81a5d7
Doha Hmeid <doha.hmeid@gmail.com>
2022-10-06 01:05:00 +0300

    Time: 97 ms (5.30%), Space: 116.6 MB (5.34%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,21 +1,38 @@
 class Solution {
     public double myPow(double x, int n) {
         
-        return Math.pow(x, n);
-    }
-}   
+        //solution1 :easy //return Math.pow(x, n);
         
-        /*
-        if(n>=0)
+        //solution2: recursion 
+        if(x==1 || n==0)
+            return 1;
+        if(x==0)
+            return 0;
+        if(n==Integer.MIN_VALUE && x>0)
+            return 0;
+        if(n==Integer.MAX_VALUE && x<0)
+            return -1;
+        
+        if(n==1)
+            return x;
+        
+        
+        
+        
+        if(n>0)
             return helper(x,n);
         else
             return 1/helper(x,n*-1) ;
     }
     
+    
+    // Tail Recursion was used to avoid Stack OverFlow error
+    double res = 1;
     public double helper(double x, int n) {
-        if(n==0)
-            return 1;
-        return x*myPow(x,--n) ;
+        if(n<=0 || res==0 )
+            return res;
+        res *= x;
+        return helper(x,--n) ;
     }
     
-}*/
+}
