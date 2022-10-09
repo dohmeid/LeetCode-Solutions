d3285af6ab6da8341d2b939139f666447739e655
Doha Hmeid <doha.hmeid@gmail.com>
2022-10-09 19:16:07 +0300

    Time: 12 ms (14.22%), Space: 54.4 MB (5.27%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<Integer, Integer> map = new HashMap<>(); //val is the key,  complement = sum-val is the value
+    public boolean findTarget(TreeNode root, int k) {
+        
+        if(root.left==null && root.right==null & root.val!=k) //1 node case
+            return false;
+    
+        //put each value and it's complement for the sum in hashap
+        fillMap(root,k);
+        
+        //check if the complement of the number exists
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int key = entry.getKey();
+            int val = entry.getValue() ;
+            if(key != val && map.containsKey(val))
+                return true;
+        }
+
+        return false;
+    }
+    
+    //DFS to traverse tree
+    public void fillMap(TreeNode t, int k){
+        if(t == null)
+            return ;
+        int c = k -t.val;
+        map.put(t.val,c);
+        fillMap(t.left,k);
+        fillMap(t.right,k);
+    }
+    
+    
+}
