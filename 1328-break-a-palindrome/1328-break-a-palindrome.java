b5d39885d8498d1c289b9a126a5fa8470d03c0cc
Doha Hmeid <doha.hmeid@gmail.com>
2022-10-11 02:01:31 +0300

    Time: 7 ms (7.18%), Space: 42.4 MB (24.04%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.java
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -1,25 +1,17 @@
 class Solution {
     public String breakPalindrome(String p) {
-        
         int n = p.length();
-        
         if( n==1 )
             return "";
-        
         if(p.charAt(0)!='a')
                 return p.substring(0, 0) + 'a' + p.substring(1);
-        
-         for(int i=0;i<n/2;i++){
-                char c = p.charAt(i);
+        for(int i=0;i<n/2;i++){
+                char c = p.charAt(i); // if not a then change it to be lexographically smallest
                 if(c != 'a'){
                     return p.substring(0, i) + 'a' + p.substring(i+1);
                 }
-         }
-                
-            
-          
-        
+         }       
+        // if we reach here, there are ONLY 'a' in palindrome string, so we should change the last character to a b
         return p.substring(0, n-1) + 'b' + p.substring(n);
-        
     }
 }
