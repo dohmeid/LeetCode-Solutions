b6dc891e23dfebedeef56c372d4f7d106c5753e1
Doha Hmeid <doha.hmeid@gmail.com>
2022-10-12 14:37:09 +0300

    Time: 15 ms (18.13%), Space: 54.6 MB (24.74%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.java
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.java
@@ -3,27 +3,12 @@ class Solution {
      
         int n = nums.length;
         Arrays.sort(nums);
-       // int a = nums[n-1];
-        //int b = nums[n-2];
-        int max = 0;
-        
         for(int i=n-1; i>=2; i--){
-            
             int a = nums[i];
             int b = nums[i-1];
             int c = nums[i-2];
-            
-            if((a+b)>c && (a+c)>b && (b+c)>a){ //to check if it is impossible to form any triangle of a non-zero area, return 0
-                int h = a+b+c;
-                if(h>max ){
-                    max = h;
-                    return max;
-                }
-            }
-           
-            
-            
-          
+            if((a+b)>c && (a+c)>b && (b+c)>a) //to check if it is impossible to form any triangle of a non-zero area, return 0
+                return a+b+c;
         }
         return 0;
     }
