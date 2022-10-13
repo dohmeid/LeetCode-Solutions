d90bba37988674205acdb3b3373295fa5a95c1be
Doha Hmeid <doha.hmeid@gmail.com>
2022-10-13 09:36:24 +0300

    Time: 1 ms (47.15%), Space: 42.2 MB (57.17%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -14,7 +14,7 @@
  * }
  */
 
-//solution1: recursive
+/*solution1: recursive 
 class Solution {
     List<Integer> sol = new ArrayList<Integer>();
     public List<Integer> inorderTraversal(TreeNode root) { //inorder: left-root-right
@@ -25,4 +25,31 @@ class Solution {
         sol = inorderTraversal(root.right);
         return sol;
     }
+}*/
+
+//solution2: iteravtive
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) { 
+        
+        List<Integer> res = new ArrayList<>();
+        if(root==null)
+            return res;
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode curr = root;
+        
+        while(curr!=null || !st.empty()){ 
+            if(curr!=null){
+                st.push(curr);
+                curr = curr.left;
+            }
+            else{
+                curr=st.pop();
+                res.add(curr.val);
+                curr=curr.right;
+            }
+        }
+        return res;
+    }
 }
+
+
