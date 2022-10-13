a7775274d9af5cf207e63a4aa524f20fb2228832
Doha Hmeid <doha.hmeid@gmail.com>
2022-10-13 08:57:13 +0300

    Time: 1 ms (49.81%), Space: 41.9 MB (75.70%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -13,7 +13,8 @@
  *     }
  * }
  */
-class Solution {
+//solution1: recursive
+/*class Solution {
     List<Integer> res = new ArrayList<>();
     public List<Integer> preorderTraversal(TreeNode root) { //pre-order : root->left->right 
         if(root==null)
@@ -23,4 +24,32 @@ class Solution {
         res = preorderTraversal(root.right);
         return res;
     }
+}*/
+
+
+//solution2: iteravtive
+class Solution {
+    
+    public List<Integer> preorderTraversal(TreeNode root) { //pre-order : root->left->right 
+        List<Integer> res = new ArrayList<>();
+        if(root==null)
+            return res;
+        
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while(!st.empty()){
+            TreeNode curr = st.pop();
+            res.add(curr.val);
+            if(curr.right!=null)
+                st.push(curr.right);
+            if(curr.left!=null)
+                st.push(curr.left);
+        }
+        return res;
+    }
 }
+
+
+
+
+
