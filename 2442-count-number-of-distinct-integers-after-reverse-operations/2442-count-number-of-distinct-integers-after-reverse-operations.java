9fc733dfd7d9372090a6c0c7495b137509319d6b
Doha Hmeid <doha.hmeid@gmail.com>
2022-10-21 22:54:28 +0300

    Time: 66 ms (99.75%), Space: 61.3 MB (96.27%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
@@ -6,8 +6,7 @@ class Solution {
             set.add(n);
             int rev = 0;
             while(n>0){
-                int rem = n%10;
-                rev = rev*10 + rem;
+                rev = rev*10 + n%10;
                 n = n/10;
             }
             set.add(rev);  
