fcb664e23805732b70a94a7dcecf471709de84c5
Doha Hmeid <doha.hmeid@gmail.com>
2022-10-25 18:46:51 +0300

    Time: 2 ms (62.71%), Space: 41.3 MB (82.75%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean arrayStringsAreEqual(String[] w1, String[] w2) {
         String res1 = "";
         String res2 = "";
-        
         for(int i=0 ; i<w1.length; i++)
             res1 += w1[i];
         for(int i=0 ; i<w2.length; i++)
