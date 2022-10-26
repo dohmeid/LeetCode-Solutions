b3d8e2732cc281a83ed6bd06a146cfb30148230a
Doha Hmeid <doha.hmeid@gmail.com>
2022-10-26 16:44:18 +0300

    Time: 68 ms (34.99%), Space: 109.9 MB (47.75%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -6,17 +6,12 @@ class Solution {
         
         for(int i=0; i<nums.length; i++){
             sum += nums[i];
-            
             if(!map.containsKey(sum % k))
                 map.put(sum % k, i + 1);
-            
-            else{
-                if(map.get(sum % k) < i){
+            else if(map.get(sum % k) < i)
                     return true;
-                }
-            }
-            
         }
+        
         return false;
     }
 }
