03aaf5d7879c5f4317509202dc6a7688b94e01e8
Doha Hmeid <doha.hmeid@gmail.com>
2022-11-03 21:34:48 +0200

    Time: 182 ms (49.26%), Space: 62.3 MB (82.10%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int longestPalindrome(String[] words) {
+        
+        Map<String, Integer> map = new HashMap<>();  
+        int res=0,flag=0;
+        for(String s : words){
+            String rev = "" + s.charAt(1) + s.charAt(0); 
+            if(map.containsKey(rev)){
+                res += 4;
+                map.put(rev, map.get(rev) - 1);
+                if(map.get(rev)==0)
+                    map.remove(rev);   
+            }
+            else{
+                int val = map.getOrDefault(s, 0) + 1;
+                map.put(s, val);
+            }
+        }
+        
+        for (Map.Entry<String, Integer> w : map.entrySet()) {
+            String s = w.getKey() ;
+            int val = w.getValue();
+            if(val!=0 && s.charAt(0)==s.charAt(1)){
+                res += 2;
+                break;
+            }
+        }
+        
+        
+        
+        
+        
+        return res;
+    }
+}
