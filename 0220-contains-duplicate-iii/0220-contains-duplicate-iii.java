5e8f47ae564a68c360bb753f7f8e3a5d8a0968c5
Doha Hmeid <doha.hmeid@gmail.com>
2022-11-06 19:18:40 +0200

    Time: 1910 ms (10.43%), Space: 75 MB (75.34%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
@@ -1,3 +1,4 @@
+
 class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
         
@@ -7,13 +8,8 @@ class Solution {
             for(int j=i+1; j<n&&j<=nn;j++){
                 if(Math.abs(nums[i]-nums[j]) <= valueDiff )
                     return true;
-            }
-            
-            
+            }   
         }
-        
-        
-        
         return false;
     }
 }
