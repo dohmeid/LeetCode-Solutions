bf287398057727c794eb800c3cb69587bd427ebe
Doha Hmeid <doha.hmeid@gmail.com>
2022-11-07 20:59:09 +0200

    Time: 1 ms (88.30%), Space: 41 MB (54.69%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int maximum69Number (int num) {
         
@@ -7,6 +8,23 @@ class Solution {
              if(n.charAt(i)=='6'){
                 n = n.substring(0, i) + '9' + n.substring(i + 1);
                 return Integer.valueOf(n);   
+             }
+            
+        }
+
+        return num;
+    }
+}*/
+
+class Solution {
+    public int maximum69Number (int num) {
+        
+        char ar[] = String.valueOf(num).toCharArray(); 
+        
+        for(int i=0; i<ar.length;i++){  
+             if(ar[i]=='6'){
+                ar[i]='9';
+                return Integer.valueOf(new String(ar));   
              }
             
         }
