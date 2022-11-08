4ec77639660b983eb2725fe12e80408844d21b99
Doha Hmeid <doha.hmeid@gmail.com>
2022-11-08 23:06:02 +0200

    Time: 9 ms (16.75%), Space: 43.1 MB (27.16%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,26 +1,21 @@
 class Solution {
     public String makeGood(String s) {
-        if(s=="" || s.length()==1)
+       if(s=="" || s.length()==1)
             return s;
        Stack<Character> st = new Stack<>();
        st.push(s.charAt(0));
         
        for(int i=1;i<s.length();i++){
            char c1 = s.charAt(i);
-           if(st.isEmpty())
-               st.push(c1);
-           else if(c1+32==st.peek() || st.peek()+32==c1 )
+           if(st.isEmpty()==false && (c1+32==st.peek() || st.peek()+32==c1 ) )
                st.pop();
            else
-               st.push(s.charAt(i));
+               st.push(c1);
       }    
-       if(st.size()==0)
-               return "";
+       
        StringBuilder res = new StringBuilder();
-       while(st.size()!=0){
+       while(st.size()!=0)
            res.append( st.pop() );
-       }
-         
-        return res.reverse().toString();
+       return res.reverse().toString();
     }
 }
