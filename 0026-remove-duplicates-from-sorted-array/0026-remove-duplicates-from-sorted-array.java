e21d6926b523fde5aba884974c6a04fb5190b893
Doha Hmeid <doha.hmeid@gmail.com>
2022-11-11 14:48:37 +0200

    Time: 2 ms (73.26%), Space: 48.1 MB (26.60%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,5 +1,22 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        
+        int k=1;
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i]!=nums[i+1]){
+                nums[k] = nums[i+1];
+                k++;
+            }
+        }
+        return k;
+    }
+}
+
+
+
+/*
+class Solution {
+    public int removeDuplicates(int[] nums) {
         Set<Integer> set = new HashSet<>();  
         for(int i: nums)
             set.add(i);
@@ -15,3 +32,4 @@ class Solution {
         return set.size();
     }
 }
+*/
