4253cd8c48d8d71d758a46f51967a04dc1c9905a
Doha Hmeid <doha.hmeid@gmail.com>
2022-11-13 22:01:04 +0200

    Time: 22 ms (35.84%), Space: 43 MB (77.75%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -3,10 +3,15 @@ class Solution {
 
         String[] s2 = s.trim().split(" +"); // (\\s+) is used to match one or more whitespace characters
         
+        /*
         String res = "";
         for(int i=s2.length-1;i>=0;i--){
                 res +=  s2[i] +" ";
         }
         return res.trim();
+        */
+        Collections.reverse(Arrays.asList(s2));
+        return String.join(" ",s2);
+        
     }
 }
