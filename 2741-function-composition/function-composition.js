0cbece414884d932722495dbaa0323b59f77a6b3
Doha Hmeid <doha.hmeid@gmail.com>
2025-03-27 11:07:01 +0200

    Time: 53 ms (75.04%) | Memory: 54.6 MB (47.23%) - LeetSync

--- 2741-function-composition/function-composition.js
+++ 2741-function-composition/function-composition.js
@@ -5,13 +5,23 @@
 var compose = (functions) => {
     
     //method1 - brute force solution
-    return function(x) {
+    /*return function(x) {
+        let result = x;
         for(let i=functions.length-1; i>=0;i--){
             let fi = functions[i];
-            x = fi(x);
+            result = fi(result);
         }
-        return x;
-        
+        return result;
+    }*/
+
+    //method2 - using reduceRight (traverse an array in reverse order)
+    return function(x) {
+       
+       let result = functions.reduceRight((res,f)=>{
+        return f(res);
+       },x);
+    
+        return result;
     }
 };
 
