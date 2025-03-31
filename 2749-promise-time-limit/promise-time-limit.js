caa7712fe2735457a9bc8deebc32a3304fcf79df
Doha Hmeid <doha.hmeid@gmail.com>
2025-04-01 00:44:07 +0300

    Time: 37 ms (97.39%) | Memory: 54.4 MB (14.73%) - LeetSync

---
+++ 2749-promise-time-limit/promise-time-limit.js
@@ -0,0 +1,32 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function (fn, t) {
+
+    return async function (...args) {
+        const myPromise = new Promise((resolve, reject) => {
+
+            const timeoutID = setTimeout(() => reject("Time Limit Exceeded"), t);
+
+            fn(...args)
+                .then((result) => {
+                    resolve(result);
+                })
+                .catch((error) => {
+                    reject(error);
+                })
+                .finally(() => {
+                    clearTimeout(timeoutID);
+                });
+        });
+
+        return myPromise;
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
