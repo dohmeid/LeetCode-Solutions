cbe63fe4c608b13e061da299091319a97cf12584
Doha Hmeid <doha.hmeid@gmail.com>
2025-04-04 22:49:00 +0300

    Time: 35 ms (96.05%) | Memory: 57.5 MB (13.71%) - LeetSync

--- 2798-chunk-array/chunk-array.js
+++ 2798-chunk-array/chunk-array.js
@@ -11,21 +11,31 @@ var chunk = function (arr, size) {
     if (size > arr.length)
         return [arr];
 
+    //method1
+    let result = [];
+    for (let i = 0; i <arr.length; i+=size) {
+        result.push(arr.slice(i,i+size));
+    }
+    return result;
+
+    /* method2 - less efficiency
+
     let temp = [];
-    let result=[];
+    let result = [];
 
     for (let item of arr) {
         temp.push(item);
 
-        if(temp.length===size){
+        if (temp.length === size) {
             result.push(temp);
-            temp=[];
+            temp = [];
         }
     }
 
-    if(temp.length!==0){
-         result.push(temp);
+    if (temp.length !== 0) {
+        result.push(temp);
     }
 
     return result;
+    */
 };
