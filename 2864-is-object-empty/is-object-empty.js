038bd314cc8f874e76f7753e666547ccef02fd33
Doha Hmeid <doha.hmeid@gmail.com>
2025-04-04 22:02:17 +0300

    Time: 43 ms (61.20%) | Memory: 53.9 MB (36.83%) - LeetSync

--- 2864-is-object-empty/is-object-empty.js
+++ 2864-is-object-empty/is-object-empty.js
@@ -4,7 +4,12 @@
  */
 var isEmpty = function(obj) {
     
-    if(Object.keys(obj).length===0)
+    //method1 - convert to JSON string and check length less than 2 - {} - the 2 for open+close braces
+    if(JSON.stringify(obj).length<=2)
         return true;
+
+    //method2 - check the keys of the object
+    //if(Object.keys(obj).length===0)
+      //  return true;
     return false;
 };
