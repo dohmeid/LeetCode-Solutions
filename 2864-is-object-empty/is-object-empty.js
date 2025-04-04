1982ff39a6d611a2d49936569d81efee2b8ef053
Doha Hmeid <doha.hmeid@gmail.com>
2025-04-04 22:08:09 +0300

    Time: 45 ms (51.20%) | Memory: 54.2 MB (17.74%) - LeetSync

--- 2864-is-object-empty/is-object-empty.js
+++ 2864-is-object-empty/is-object-empty.js
@@ -2,14 +2,18 @@
  * @param {Object|Array} obj
  * @return {boolean}
  */
-var isEmpty = function(obj) {
-    
-    //method1 - convert to JSON string and check length less than 2 - {} - the 2 for open+close braces
-    if(JSON.stringify(obj).length<=2)
-        return true;
+var isEmpty = function (obj) {
 
-    //method2 - check the keys of the object
-    //if(Object.keys(obj).length===0)
-      //  return true;
-    return false;
+    /*method1 - convert to JSON string and check length less than 2 - {} - the 2 for open+close braces
+    return JSON.stringify(obj).length<=2;
+    */
+
+    /*method2 - check the keys of the object
+    return Object.keys(obj).length===0;
+    */
+
+    //method3 - check if the obj can enter a loop (aka it's not empty)
+    for (let key in obj)
+        return false;
+    return true;
 };
